d9cbffc6674ea87ad37e7dbfb2908735d177969a
sylene <sylene.manusset@gmail.com>
2020-08-24 11:19:07 +0200

    Add firebase to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-library-angular';
+  constructor() {
+    var firebaseConfig = {
+      apiKey: 'AIzaSyC8kXXzVK8envSqBnifJp-4YpNWY5eGakg',
+      authDomain: 'my-library-angular.firebaseapp.com',
+      databaseURL: 'https://my-library-angular.firebaseio.com',
+      projectId: 'my-library-angular',
+      storageBucket: 'my-library-angular.appspot.com',
+      messagingSenderId: '738815355200',
+      appId: '1:738815355200:web:2b57d022347c031bdef2a5',
+      measurementId: 'G-R9H8Z5FHXV',
+    };
+
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  }
 }
