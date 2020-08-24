7eb6b07b3aa211b1a0f687bf92e2a2264cdb1afe
sylene <sylene.manusset@gmail.com>
2020-08-24 15:03:24 +0200

    add signout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  isAuth: boolean;
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    firebase.auth().onAuthStateChanged((user) => {
+      user ? (this.isAuth = true) : (this.isAuth = false);
+    });
   }
 
+  onSignOut() {
+    this.authService.signOutUser();
+  }
 }
