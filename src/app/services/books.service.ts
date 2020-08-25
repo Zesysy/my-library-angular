117020f421e34398b6e791271764cba22ea4984d
sylene <sylene.manusset@gmail.com>
2020-08-25 16:38:43 +0200

    Add methods to save, create, get and delete a book in BooksService

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,67 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import * as firebase from 'firebase';
+
+import { Book } from '../models/book.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BooksService {
+  books: Book[] = [];
+  booksSubject = new Subject<Book[]>();
 
-  constructor() { }
+  constructor() {}
+
+  emitBooks() {
+    this.booksSubject.next(this.books);
+  }
+
+  saveBooks() {
+    firebase.database().ref('/books').set(this.books);
+  }
+
+  getAllBooks() {
+    firebase
+      .database()
+      .ref('/books')
+      .on('value', (data) => {
+        this.books = data.val() ? data.val() : [];
+        this.emitBooks();
+      });
+  }
+
+  getOneBook(id: number) {
+    return new Promise((resolve, reject) => {
+      firebase
+        .database()
+        .ref('/books/' + id)
+        .once('value')
+        .then(
+          (data) => {
+            resolve(data.val());
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+    });
+  }
+
+  createNewBook(newBook: Book) {
+    this.books.push(newBook);
+    this.saveBooks();
+    this.emitBooks();
+  }
+
+  removeBook(book: Book) {
+    const bookIndexToRemove = this.books.findIndex((bookEl) => {
+      if (bookEl === book) {
+        return true;
+      }
+    });
+    this.books.splice(bookIndexToRemove, 1);
+    this.saveBooks();
+    this.emitBooks();
+  }
 }
